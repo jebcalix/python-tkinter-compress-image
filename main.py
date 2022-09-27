b416d62eef8b47c5e87fc8ee655b4efe1ac428bb
jbonilla <jbonilla@greenpay.me>
2022-09-26 22:37:24 -0600

    Added tkinter interface

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-import PIL
 from PIL import Image
 from tkinter.filedialog import *
+from tkinter import Tk, Button
 
-file_path = askopenfilename()
-img = Image.open(file_path)
+class Compress:
+	def __init__(self, master):
+		master.title("Compresor de Imagenes")
+		master.geometry("400x400+400+400")
+		master.config(bg='gray')
+		master.resizable(False, False)
+		Button(width=3, height=1,font=('Arial Bold', 18), text='Abrir', relief='groove', bg='white',command=self.comprimir).place(x=175, y=175)
+	def comprimir(self):
+		file_path = askopenfilename(filetypes=[('Images', '.jpg .jpeg')])
+		img = Image.open(file_path)
 
-myH, myW = img.size
+		myH, myW = img.size
 
-img = img.resize((myH,myW), Image.LANCZOS)
+		img = img.resize((myH,myW), Image.ANTIALIAS)
 
-save_path = asksaveasfilename()
+		save_path = asksaveasfilename(filetypes=[('Images', '.jpg .jpeg')])
 
-img.save(save_path + '_compressed.jpg')
+		img.save(save_path + '_compressed.jpg', optimize=True, quality=70)
+
+root = Tk()
+compress=Compress(root)
+root.mainloop()
